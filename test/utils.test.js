b7d2dd9b7bd6a65ad0b59f6525b235fdbef56c88
Ionut Predoi <ipredoi27@gmail.com>
2024-01-22 16:26:08 +0000

    Revert "[DSI-6605] Exclude devices API dependency checks for all environments"

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,7 +2,7 @@ const rp = require('login.dfe.request-promise-retry');
 
 const {
   CONFIG_DEVICES_API_KEY,
-
+  PROD_URL_KEYWORDS,
   CONFIG_API_TYPE,
   CONFIG_IDENTIFYING_PARTY_SECTION,
   CONFIG_OIDC_SECTION,
@@ -10,6 +10,7 @@ const {
 } = require('../constants/constants');
 const {
   trimTrailingSlash,
+  shouldCheckDevicesApi,
   getApiUrl,
   checkDependentServiceHealth,
   getIdentifyingPartyConfig,
@@ -47,6 +48,33 @@ describe('utils', () => {
     });
   });
 
+  describe('shouldCheckDevicesApi function', () => {
+    it(`should return true when key is not '${CONFIG_DEVICES_API_KEY}'`, () => {
+      const result = shouldCheckDevicesApi('dsi-test-api', { url: 'http://dsi-test-api.com' });
+      expect(result).toBe(true);
+    });
+
+    it(`should return false when key is ${CONFIG_DEVICES_API_KEY} but URL does not include '${PROD_URL_KEYWORDS.join(', ')}' keywords`, () => {
+      const result = shouldCheckDevicesApi(CONFIG_DEVICES_API_KEY, { url: 'http://example.dev' });
+      expect(result).toBe(false);
+    });
+
+    it(`should return true when key is ${CONFIG_DEVICES_API_KEY} and URL includes '${PROD_URL_KEYWORDS.join(', ')}' keywords`, () => {
+      const result = shouldCheckDevicesApi(CONFIG_DEVICES_API_KEY, { url: 'http://p01-devices-api.com' });
+      expect(result).toBe(true);
+    });
+
+    it('should return false when service is undefined', () => {
+      const result = shouldCheckDevicesApi(CONFIG_DEVICES_API_KEY, undefined);
+      expect(result).toBe(false);
+    });
+
+    it('should return false when service url is undefined', () => {
+      const result = shouldCheckDevicesApi(CONFIG_DEVICES_API_KEY, { url: undefined });
+      expect(result).toBe(false);
+    });
+  });
+
   describe('getApiUrl function', () => {
     it('should return null when value is not an object', () => {
       const value = 'string-value';
@@ -66,9 +94,14 @@ describe('utils', () => {
       expect(result).toBe(null);
     });
 
-    it(`should return null when key is ${CONFIG_DEVICES_API_KEY}`, () => {
+    it('should return null when shouldCheckDevicesApi is false', () => {
       const value = { type: CONFIG_API_TYPE, service: { url: 'http://test-api-service.com' } };
-      const result = getApiUrl(CONFIG_DEVICES_API_KEY, value);
+      const result = getApiUrl('devices', value);
+
+      expect(result).toBe(null);
+    });
+    it('should return null when shouldCheckDevicesApi is true but service is not defined', () => {
+      const result = getApiUrl('dsi-test-api', { type: CONFIG_API_TYPE });
 
       expect(result).toBe(null);
     });
