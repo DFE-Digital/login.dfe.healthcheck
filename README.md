961d4578318fe9bcd88d56ad15ed8665b3f3b650
Julio Velezmoro <julio28ve@gmail.com>
2025-04-16 13:54:59 +0100

    Format with `format` command.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # DfE Express Health Check
+
 [![tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)](https://github.com/facebook/jest)
 
 Express middleware to provide health check endpoints
--- test/harness.js
+++ test/harness.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const healthcheck = require('./../lib');
+const express = require("express");
+const healthcheck = require("./../lib");
 
 const app = express();
 
-app.use('/healthcheck', healthcheck({
-  config: {
-    redis: {
-      good: {
-        connectionString: 'redis://localhost:6379',
+app.use(
+  "/healthcheck",
+  healthcheck({
+    config: {
+      redis: {
+        good: {
+          connectionString: "redis://localhost:6379",
+        },
+        bad: {
+          connectionString: "redis://nosuchhost:1234",
+        },
       },
-      bad: {
-        connectionString: 'redis://nosuchhost:1234',
-      },
-    }
-  },
-}));
+    },
+  }),
+);
 
 app.listen(3000, () => {
-  console.info('Healthcheck mounted at http://localhost:3000/healthcheck');
+  console.info("Healthcheck mounted at http://localhost:3000/healthcheck");
 });
